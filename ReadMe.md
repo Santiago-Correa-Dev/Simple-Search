3782dd4a18731b2a365fdb309d921c3e19e93846
Santiago Correa Espinosa <santiago.correa.dev@gmail.com>
2017-10-17 17:02:11 -0500

    Initial Commit
    Upload of files.

---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+# Simple Search Engine (UpSplash and NY Times)
+
+## Developers - How to make use of this code
+* Create Your Accounts
+- You'll need accounts with Unsplash and The New York Times.
+- Create a developer account here - https://unsplash.com/developers
+- Next, create an application here - https://unsplash.com/oauth/applications
+- this will give you an "Application ID" that you'll need to make requests
+* The New York Times
+- Create a developer account here - https://developer.nytimes.com/
+- They'll email you your api-key (you'll need this to make requests)
+* Download the repository and change the object properties for either your own variables or own object properties in the app.js file.
+
+## Users
+
+* Just download the repository and take a look at some cool searches.
+* Not all images will be populated , only one image will but many NY times articles will be populated.
+* Why only one picture, because this is just for you to have fun and search.
+
+### Where does the information come from?
+* UpSplash 
+* NY times.
+
+#### How?
+
+The use of API's and documentation.
---
+++ app.js
@@ -0,0 +1,53 @@
+(function() {
+    let form = document.getElementById('form');
+    let submit = document.getElementById('submit');
+    let searchText = document.getElementById('search');
+    let images = document.getElementById('images-area');
+    let articlearea = document.getElementById('articles-area');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        searchedForText = searchText.value;
+        images.innerHTML = '';
+
+
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
+                headers: {
+                    Authorization: `Client-ID ${api.apiUp}`
+                }
+            })
+            .then(response => response.json())
+            .then(addImage)
+            .catch(e => requestError(e, "images"));
+
+
+        function addImage(data) {
+            const firstImage = data.results[0];
+            if (firstImage) {
+                htmlContent = `<figure><img src="${firstImage.urls.regular}" alt="${searchedForText}"><figcaption>${searchedForText} by ${firstImage.user.name}</figure>`;
+            } else {
+                htmlContent = `<div class="error-no-image">Unfortunately no image popped up for your search : ${searchedForText}</div`;
+            }
+            images.insertAdjacentHTML('afterbegin', htmlContent);
+        };
+
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&page=1&sort=newest&api-key=${api.apiNy}`)
+            .then(response => response.json())
+            .then(addArticles)
+            .catch(e => requestError(e, "articles"));
+
+        function addArticles(data) {
+            let articles = data.response.docs;
+            if (articles.length > 1) {
+                htmlContent = '<ul>' + articles.map(article => `<li class="article"><a href="${article.web_url}">${article.headline.main}</a><p>${article.snippet}</p></li>`).join('') + '</ul>';
+            } else {
+                htmlContent = `<ul><li class="error-no-articles>No articles came up by searching for ${searchedForText}</li></ul>"`;
+            }
+            images.insertAdjacentHTML('beforeend', htmlContent);
+        };
+
+        function requestError(e, part) {
+            images.insertAdjacentHTML('beforeend', `<div class="error-no-articles">Something happend with the network. No ${part} were loaded </div>`);
+        };
+    });
+})();
